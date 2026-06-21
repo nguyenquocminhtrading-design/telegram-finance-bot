cb935d53a13612dcc69ba9322e0fac5ae1e64c3d
nguyenquocminhtrading-design <nguyenquocminh.trading@gmail.com>
2026-06-21 07:22:05 +0700

    Auto deploy update

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,25 @@ echo ==============================================
 echo  TELEGRAM FINANCE BOT - AUTO DEPLOY SCRIPT
 echo ==============================================
 
+if exist .git goto deploy
+
+echo [0/4] Khoi tao Git repository lan dau...
+git init
+git branch -M main
+git add .
+git commit -m "Initial commit"
+
+echo [1/4] Dang tao GitHub repository tu dong qua GitHub CLI...
+for %%I in (.) do set RepoName=%%~nxI
+echo Ten repository se tao: %RepoName%
+
+gh repo create %RepoName% --private --source=. --remote=origin --push
+
+echo ==============================================
+echo [Thanh cong] Da khoi tao va day code len GitHub repository: %RepoName%!
+goto finish
+
+:deploy
 echo [1/3] Adding files to Git...
 git add .
 
@@ -15,6 +34,7 @@ git commit -m "%commitMsg%"
 echo [3/3] Pushing to GitHub...
 git push origin main
 
+:finish
 echo ==============================================
 echo Deploy script finished!
 echo Now, to update PythonAnywhere:
